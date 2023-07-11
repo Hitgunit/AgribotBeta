680b4818aef6e5ed95b1a0c71e19b4b689d5ba8d
Adair Duarte <ada_dp10@hotmail.com>
2023-07-11 09:21:38 -0600

    conexion con Firebase

--- app/src/main/java/com/example/agribotbeta/MainActivity.kt
+++ app/src/main/java/com/example/agribotbeta/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.agribotbeta
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class MainActivity : AppCompatActivity() {
+
+    var db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Create a new user with a first, middle, and last name
+        // Create a new user with a first, middle, and last name
+        val user: MutableMap<String, Any> = HashMap()
+        user["first"] = "Alan"
+        user["middle"] = "Mathison"
+        user["last"] = "Turing"
+        user["born"] = 1912
+
+// Add a new document with a generated ID
+
+// Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(
+                    TAG,
+                    "DocumentSnapshot added with ID: " + documentReference.id
+                )
+            }
+            .addOnFailureListener { e -> Log.w(TAG, "Error adding document", e) }
     }
 }
