556825abbbaf8da28888e86a0ef5bf52a618cc33
Adair Duarte <ada_dp10@hotmail.com>
2023-07-12 08:40:31 -0600

    Modificación .xml definitivo
    Se modificaron ambos .xml para poder tener las mejores vistas

--- app/src/main/java/com/example/agribotbeta/MainActivity.kt
+++ app/src/main/java/com/example/agribotbeta/MainActivity.kt
@@ -14,25 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Create a new user with a first, middle, and last name
-        // Create a new user with a first, middle, and last name
-        val user: MutableMap<String, Any> = HashMap()
-        user["first"] = "Alan"
-        user["middle"] = "Mathison"
-        user["last"] = "Turing"
-        user["born"] = 1912
 
-// Add a new document with a generated ID
-
-// Add a new document with a generated ID
-        db.collection("users")
-            .add(user)
-            .addOnSuccessListener { documentReference ->
-                Log.d(
-                    TAG,
-                    "DocumentSnapshot added with ID: " + documentReference.id
-                )
-            }
-            .addOnFailureListener { e -> Log.w(TAG, "Error adding document", e) }
     }
 }
