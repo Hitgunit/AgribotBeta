9d525385c602cd5f94af3dd24855ba21a7f123ef
Adair Duarte <ada_dp10@hotmail.com>
2023-07-12 09:42:16 -0600

    Division Mensajes .xml
    Se dividió el mensaje de usuario y el bot en dos .xml distinto

--- app/src/main/java/com/example/agribotbeta/MessageAdapter.kt
+++ app/src/main/java/com/example/agribotbeta/MessageAdapter.kt
@@ -9,16 +9,33 @@ import androidx.recyclerview.widget.RecyclerView
 class MessageAdapter(private val messages: List<Mensaje>) :
     RecyclerView.Adapter<MessageAdapter.MessageViewHolder>() {
 
+    companion object {
+        private const val VIEW_TYPE_BOT = 1
+        private const val VIEW_TYPE_USER = 2
+    }
+
     inner class MessageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val tvMessage: TextView = view.findViewById(R.id.tv_message)
-        val tvBotMessage: TextView = view.findViewById(R.id.tv_bot_message)
+        val tvMessage: TextView? = view.findViewById(R.id.tv_message)
+        val tvBotMessage: TextView? = view.findViewById(R.id.tv_bot_message)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.message_item, parent, false)
-
+        val view = if (viewType == VIEW_TYPE_BOT) {
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.message_item_bot, parent, false)
+        } else {
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.message_item_user, parent, false)
+        }
         return MessageViewHolder(view)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (messages[position].esBot) {
+            VIEW_TYPE_BOT
+        } else {
+            VIEW_TYPE_USER
+        }
     }
 
     override fun getItemCount(): Int {
@@ -28,13 +45,10 @@ class MessageAdapter(private val messages: List<Mensaje>) :
     override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
         val message = messages[position]
         if (message.esBot) {
-            holder.tvMessage.visibility = View.GONE
-            holder.tvBotMessage.visibility = View.VISIBLE
-            holder.tvBotMessage.text = message.texto
+            holder.tvBotMessage?.text = message.texto
         } else {
-            holder.tvMessage.visibility = View.VISIBLE
-            holder.tvBotMessage.visibility = View.GONE
-            holder.tvMessage.text = message.texto
+            holder.tvMessage?.text = message.texto
         }
     }
 }
+
