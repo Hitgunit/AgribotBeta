9e1213796350bc1c0d36b6c15b306dbac8fc6918
Adair Duarte <ada_dp10@hotmail.com>
2023-07-14 13:21:13 -0600

    LecturaFirestore
    Se modifico el fun GenerarRespuestaBot para poder comparar la respuesta con una palabra clave en la base de datos y traer la respuesta

--- app/src/main/java/com/example/agribotbeta/MainActivity.kt
+++ app/src/main/java/com/example/agribotbeta/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.agribotbeta
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+
+    //Variable de conexion firebase
+    private val db = FirebaseFirestore.getInstance()
 
     // Esta es la lista de mensajes que inicialmente está vacía
     val mensajes = mutableListOf<Mensaje>()
@@ -41,20 +47,52 @@ class MainActivity : AppCompatActivity() {
 
     private fun enviarMensaje(texto: String) {
         mensajes.add(Mensaje(texto, false))
-        val respuestaBot = generarRespuestaBot(texto)
-        mensajes.add(Mensaje(respuestaBot, true))
+        generarRespuestaBot(texto)
         adaptadorRecyclerView?.notifyDataSetChanged()
-
         // Agregamos la función de desplazamiento al último mensaje después de cada envío
         rvMessages.scrollToPosition(mensajes.size - 1)
     }
 
-    private fun generarRespuestaBot(texto: String): String {
-        return when {
-            texto.contains("Hola") -> "Hello"
-            texto.contains("Quien es?") -> "Un bot bebe"
-            else -> "Lo siento, no entiendo tu problema. Por favor proporciona más detalles."
-        }
+
+    private fun generarRespuestaBot(texto: String) {
+        db.collection("Problemas")
+            .get()
+            .addOnSuccessListener { result ->
+                var foundKeyword = false // Variable para controlar si encontramos una palabra clave
+                loop@ for (document in result) {
+                    val palabrasClave = document.get("palabras clave") as List<String>
+                    for (palabraClave in palabrasClave) {
+                        if (texto.contains(palabraClave)) {
+                            foundKeyword = true // Encontramos una palabra clave
+                            val soluciones = (document.get("soluciones") as List<Map<String, Any>>).sortedBy { (it["orden"] as String).toInt() }
+                            for (solucion in soluciones) {
+                                // Aquí tienes el texto de la solución
+                                val solucionTexto = solucion["texto"] as String
+                                // Agrega la solución como un mensaje del bot
+                                mensajes.add(Mensaje(solucionTexto, true))
+                                adaptadorRecyclerView?.notifyDataSetChanged()
+                                rvMessages.scrollToPosition(mensajes.size - 1)
+                            }
+                            // Saliendo del loop una vez que se ha encontrado la palabra clave en el texto
+                            break@loop
+                        }
+                    }
+                }
+                // Si no encontramos ninguna palabra clave, agregamos un mensaje predeterminado
+                if (!foundKeyword) {
+                    mensajes.add(Mensaje("Lo siento, no puedo ayudarte con eso.", true))
+                    adaptadorRecyclerView?.notifyDataSetChanged()
+                    rvMessages.scrollToPosition(mensajes.size - 1)
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
     }
+
+
+
+
+
 }
 
