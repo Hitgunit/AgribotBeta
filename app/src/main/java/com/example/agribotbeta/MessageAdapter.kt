559c8e234f5952ac7545e5d0750880bf3febefa2
Adair Duarte <ada_dp10@hotmail.com>
2023-07-20 17:04:29 -0600

    SeleccionarImagen
    Ahora la imagen se puede seleccionar para visualizarla mejor

--- app/src/main/java/com/example/agribotbeta/MessageAdapter.kt
+++ app/src/main/java/com/example/agribotbeta/MessageAdapter.kt
@@ -7,6 +7,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
 
 class MessageAdapter(private val messages: List<Mensaje>) :
     RecyclerView.Adapter<MessageAdapter.MessageViewHolder>() {
@@ -54,11 +56,28 @@ class MessageAdapter(private val messages: List<Mensaje>) :
                     .load(message.imagen).fitCenter()
                     .into(holder.ivBotImage!!)
                 holder.ivBotImage.visibility = View.VISIBLE
+                holder.ivBotImage.setOnClickListener {
+                    showImageDialog(holder.ivBotImage.context, message.imagen)
+                }
             } else {
                 holder.ivBotImage?.visibility = View.GONE
             }
         } else {
             holder.tvMessage?.text = message.texto
         }
+    }
+
+    private fun showImageDialog(context: Context, imageUrl: String) {
+        val builder = AlertDialog.Builder(context)
+        val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_image, null)
+        val imageView = dialogView.findViewById<ImageView>(R.id.dialog_image_view)
+
+        Glide.with(context)
+            .load(imageUrl).fitCenter()
+            .into(imageView)
+
+        builder.setView(dialogView)
+            .setPositiveButton(android.R.string.ok) { dialog, _ -> dialog.dismiss() }
+        builder.create().show()
     }
 }
